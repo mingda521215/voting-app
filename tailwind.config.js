a3fe57ed6d079a286745199fb2ee8771dab74110
Chao mini99 <chaomini99@ChaodeMac-mini.local>
2023-02-17 00:27:11 +0800

    Episode 3 - Design Idea Cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    purge: [
+    content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -16,7 +16,7 @@ module.exports = {
 
                 black: colors.black,
                 white: colors.white,
-                gray: colors.trueGray,
+                gray: colors.gray,
                 'gray-background': '#f7f8fc',
                 'blue': '#328af1',
                 'blue-hover': '#2879bd',
@@ -26,14 +26,22 @@ module.exports = {
                 'purple' : '#8b60ed',
             },
             spacing: {
+                44: '11rem',
                 70: '17.5rem',
                 175: '43.75rem',
             },
             maxWidth: {
                 custom: '62.5rem',
             },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
+            },
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xxs: ['0.625rem', {lineHeight: '1rem'}],
             },
         },
     },
@@ -44,5 +52,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/forms'),
+    ],
 };
