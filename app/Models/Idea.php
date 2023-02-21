b859e816305a3ef15335d6b1a3a4456b2b07a921
mingda521215 <mingda@ms10.hinet.net>
2023-02-21 11:53:00 +0800

    Episode 16 - Voting for Ideas

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -47,4 +47,31 @@ class Idea extends Model
     {
         return $this->belongsToMany(User::class, 'votes');
     }
+
+    public function isVotedByUser(?User $user)
+    {
+        if (!$user) {
+            return false;
+        }
+        
+        return Vote::where('user_id', $user->id)
+            ->where('idea_id', $this->id)
+            ->exists();
+    }
+
+    public function vote(User $user)
+     {
+         Vote::create([
+             'idea_id' => $this->id,
+             'user_id' => $user->id,
+         ]);
+     }
+
+    public function removeVote(User $user)
+     {
+         Vote::where('idea_id', $this->id)
+             ->where('user_id', $user->id)
+             ->first()
+             ->delete();
+     }
 }
